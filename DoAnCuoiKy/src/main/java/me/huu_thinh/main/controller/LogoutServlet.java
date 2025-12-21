f5284e761762e2081b2470165659c6bb8b4a0fda
tantai170804 <22130239@st.hcmafu.edu.vn>
2025-12-21 20:19:57 +0700

    chuc nang logout

---
+++ DoAnCuoiKy/src/main/java/me/huu_thinh/main/controller/LogoutServlet.java
@@ -0,0 +1,29 @@
+package me.huu_thinh.main.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false); 
+        // false: không tạo session mới
+
+        if (session != null) {
+            session.invalidate(); // Hủy session
+        }
+
+        // Chuyển về trang login hoặc home
+        response.sendRedirect("login.jsp"); 
+        // hoặc response.sendRedirect("home");
+    }
+}
