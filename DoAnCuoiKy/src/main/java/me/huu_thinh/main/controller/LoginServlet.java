8c3b4102a08ab23f2ce1e052adaef55285ba1afd
tantai170804 <22130239@st.hcmafu.edu.vn>
2025-12-21 12:53:46 +0700

    chuc nang login

---
+++ DoAnCuoiKy/src/main/java/me/huu_thinh/main/controller/LoginServlet.java
@@ -0,0 +1,49 @@
+package me.huu_thinh.main.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import me.huu_thinh.main.model.User;
+import me.huu_thinh.main.model.service.LoginService;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	private final LoginService loginService = new LoginService();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/html/login.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		User user = loginService.login(username, password);
+
+		if (user != null) {
+			HttpSession session = request.getSession(true);
+			session.setAttribute("currentUser", user);
+			response.sendRedirect(request.getContextPath() + "/home");
+		} else {
+			request.setAttribute("error", "Sai username hoặc password");
+			request.setAttribute("username", username); // để giữ lại username
+			request.getRequestDispatcher("/html/login.jsp").forward(request, response);
+		}
+	}
+}
